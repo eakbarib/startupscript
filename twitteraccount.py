a4921dd1c4fe9bf6e8123ab3d45d5d68a99105d8
unknown <samadley69@gmail.com>
2023-02-01 16:13:21 -0800

    First working implementation
    A very basic implementation of a
    twitter bot that uses OpenAI API
    to fetch completetion and posts it
    as a thread to Twitter using Tweepy library

--- twitteraccount.py
+++ twitteraccount.py
@@ -24,7 +24,6 @@ class TwitterAccount:
         if(kwargs.get("filename") != None):
             filename = kwargs.get("filename")
             self.loadcredentials_from_file(filename)
-            self.init_client()
             return
         elif(kwargs.get("bearer_token") != None and kwargs.get("consumer_token") != None and kwargs.get("consumer_secret") != None and kwargs.get("access_token") != None and kwargs.get("access_token_secret") != None):
             self.bearer_token = kwargs.get("bearer_token")
@@ -38,7 +37,6 @@ class TwitterAccount:
         if(len(args) == 1):
             filename = args[0]
             self.loadcredentials_from_file(filename)
-            self.init_client()
             return
         elif(len(args) == 5):
             self.bearer_token = args[0]
@@ -50,6 +48,10 @@ class TwitterAccount:
             return
         else:
             raise Exception("Invalid Arguments")
+
+
+
+
 
     # loads the credentials from a json file and initializes the TwitterAccount
     # params: filename - the filename of the json file containing the credentials
@@ -64,8 +66,60 @@ class TwitterAccount:
             self.access_token_secret = data["access_token_secret"]
             self.init_client()
 
+
+    # utility method to initialize the client member variable
+    # initializes the client for the TwitterAccount using the credentials
+    # params: bearer_token - the bearer token for the Twitter Developer Account
+    #         consumer_token - the consumer token for the Twitter Developer Account
+    #         consumer_secret - the consumer secret for the Twitter Developer Account
+    #         access_token - the access token for the Twitter Developer Account
+    #         access_token_secret - the access token secret for the Twitter Developer Account
+
     def init_client(self):
         self.client = tweepy.Client(bearer_token=self.bearer_token, consumer_key=self.consumer_token, consumer_secret=self.consumer_secret, access_token=self.access_token, access_token_secret=self.access_token_secret,wait_on_rate_limit=True)
+
+
+
+
+    
+    # post a tweet to twitter account
+    # returns the response from twitter
+    # if the response has errors, the tweet was not successful
+    # if the response has no errors, the tweet was successful
+    # params: tweet - the tweet to post
+    # returns: the response from twitter
+    # Example:
+    # bot = TwitterBot(twitter_account,openai_key)
+    # response = bot.post_tweet("Hello World!")
+    # if response.errors == None:
+    #     print("Tweet was successful")
+    # else:
+    #     print("Tweet was not successful")
+
+    def post_tweet(self, tweet, reply_tweet_id=None):
+        return self.client.create_tweet(text=tweet, in_reply_to_tweet_id=reply_tweet_id)
+
+
+
+       # posts a list of tweets to twitter account
+    # params: tweet_series - a list of tweets to post (strings)
+    # Example:
+    # bot = TwitterBot(twitter_account,openai_key)
+    # bot.post_tweet_series(["Hello","World!","This is a tweet series","Goodbye!"])
+    # tweets:
+    # Hello
+    # World!
+    # This is a tweet series
+    # Goodbye!
+
+    def post_tweet_thread(self, tweet_series):
+        # stores the id of the last tweet posted so we can reply to it
+        reply_tweet_id = None
+        for tweet in tweet_series:
+            # if the tweet is not empty post it otherwise skip it
+            if(tweet != ""):
+                reply_tweet_id = self.post_tweet(tweet, reply_tweet_id).data['id']
+
 
 
     # deletes all the tweets from the account
